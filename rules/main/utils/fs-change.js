a2e40e11b9e48c365d632fc777fea4e56ce8a8e9
chongqiangchen <chongqiangchen@gmail.com>
2021-04-19 23:59:04 +0800

    fix: 修正fs-change路径错误

--- rules/main/utils/fs-change.js
+++ rules/main/utils/fs-change.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const md5 = require('md5');
+const { isTestEnv } = require('../../../utils/env');
+const { resolve } = require('path');
 
 let preMd5Map = new Map();
+let rootPath = isTestEnv ? '' : resolve(__dirname, '../../../../../');
 
-const isFileChange = (filePath) => {
+const isFileChange = (path) => {
+  const filePath = resolve(rootPath, path);
   let curMd5 = md5(fs.readFileSync(filePath));
   if (curMd5 !== preMd5Map.get(filePath)) {
     preMd5Map.set(filePath, curMd5);
@@ -14,7 +18,7 @@ const isFileChange = (filePath) => {
 
 const isFilesChange = (filePaths) => {
   return filePaths.every(path => isFileChange(path));
-}
+};
 
 module.exports = {
   isFileChange,
